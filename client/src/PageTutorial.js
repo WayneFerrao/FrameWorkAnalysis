6d53021e9769112cf808118eb0eb41dff0f9f5f2
Wayne Ferrao <wayne.ferrao93@gmail.com>
2018-11-13 12:05:42 -0500

    Add files via upload

--- client/src/PageTutorial.js
+++ client/src/PageTutorial.js
@@ -1,12 +1,128 @@
 import React, {Component} from 'react';
+import Particles from 'react-particles-js';
 import styled from 'styled-components';
 
+const Background = styled.div`
+background-color: #4285f4;
+`;
+
 export default class PageTutorial extends Component {
-    render(){
-        return(
-            <div>
-                This is how you make a page using React and NodeJS
-            </div>
+    render() {
+        return (
+            <Background>
+                <Particles
+                    params={{
+                        "particles": {
+                            "number": {
+                                "value": 80,
+                                "density": {
+                                    "enable": true,
+                                    "value_area": 800
+                                }
+                            },
+                            "color": {
+                                "value": "#ffffff"
+                            },
+                            "shape": {
+                                "type": "circle",
+                                "stroke": {
+                                    "width": 0,
+                                    "color": "#000000"
+                                },
+                                "polygon": {
+                                    "nb_sides": 4
+                                },
+                                "image": {
+                                    "src": "img/github.svg",
+                                    "width": 100,
+                                    "height": 100
+                                }
+                            },
+                            "opacity": {
+                                "value": 0.5,
+                                "random": false,
+                                "anim": {
+                                    "enable": false,
+                                    "speed": 1,
+                                    "opacity_min": 0.1,
+                                    "sync": false
+                                }
+                            },
+                            "size": {
+                                "value": 4.008530152163807,
+                                "random": true,
+                                "anim": {
+                                    "enable": false,
+                                    "speed": 19.489853095232284,
+                                    "size_min": 2.436231636904035,
+                                    "sync": false
+                                }
+                            },
+                            "line_linked": {
+                                "enable": false,
+                                "distance": 144.30708547789706,
+                                "color": "#ffffff",
+                                "opacity": 0.4,
+                                "width": 1
+                            },
+                            "move": {
+                                "enable": true,
+                                "speed": 8.017060304327615,
+                                "direction": "bottom",
+                                "random": false,
+                                "straight": false,
+                                "out_mode": "out",
+                                "bounce": false,
+                                "attract": {
+                                    "enable": false,
+                                    "rotateX": 1202.559045649142,
+                                    "rotateY": 1200
+                                }
+                            }
+                        },
+                        "interactivity": {
+                            "detect_on": "canvas",
+                            "events": {
+                                "onhover": {
+                                    "enable": false,
+                                    "mode": "grab"
+                                },
+                                "onclick": {
+                                    "enable": true,
+                                    "mode": "bubble"
+                                },
+                                "resize": true
+                            },
+                            "modes": {
+                                "grab": {
+                                    "distance": 400,
+                                    "line_linked": {
+                                        "opacity": 1
+                                    }
+                                },
+                                "bubble": {
+                                    "distance": 950.1303383925738,
+                                    "size": 16.241544246026905,
+                                    "duration": 1.0557003759917487,
+                                    "opacity": 0.12181158184520177,
+                                    "speed": 3
+                                },
+                                "repulse": {
+                                    "distance": 200,
+                                    "duration": 0.4
+                                },
+                                "push": {
+                                    "particles_nb": 4
+                                },
+                                "remove": {
+                                    "particles_nb": 2
+                                }
+                            }
+                        },
+                        "retina_detect": true
+                    }}
+                />
+            </Background>
         )
     }
 
