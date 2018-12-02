60013fa794ff5edf3e9f33384bb0057eecbfa934
Wayne Ferrao <wayne.ferrao93@gmail.com>
2018-12-02 02:31:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
