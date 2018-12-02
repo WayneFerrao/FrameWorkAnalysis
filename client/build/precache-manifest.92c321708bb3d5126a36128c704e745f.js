b907ee9a3dce2b7a2f36efc1ce4128639ef66f2a
Wayne Ferrao <wayne.ferrao93@gmail.com>
2018-12-02 02:17:29 -0500

    run build

---
+++ client/build/precache-manifest.92c321708bb3d5126a36128c704e745f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac3a4c27d5589ad38a49",
+    "url": "/static/css/main.5810996e.chunk.css"
+  },
+  {
+    "revision": "ac3a4c27d5589ad38a49",
+    "url": "/static/js/main.ac3a4c27.chunk.js"
+  },
+  {
+    "revision": "ab8a9caac54400a11466",
+    "url": "/static/js/1.ab8a9caa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/reactImg.580ffd26.png"
+  },
+  {
+    "revision": "270f55fffea36219b66a63fbe03ff497",
+    "url": "/static/media/nodejs.270f55ff.png"
+  },
+  {
+    "revision": "87cbe4e12651ab82ed1937369591e340",
+    "url": "/static/media/meirl.87cbe4e1.png"
+  },
+  {
+    "revision": "9d9a88ff5cc808f75e252c0d84beb139",
+    "url": "/static/media/brandone.9d9a88ff.png"
+  },
+  {
+    "revision": "c2401c3a3ce6b250b2508decd17270f3",
+    "url": "/static/media/json.c2401c3a.png"
+  },
+  {
+    "revision": "45e1144b7c1cf99f0983087ed6b73325",
+    "url": "/static/media/wane.45e1144b.png"
+  },
+  {
+    "revision": "045c973f3377f0bff01b6610c17d3583",
+    "url": "/index.html"
+  }
+];
