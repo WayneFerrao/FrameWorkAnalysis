da5b0a5287f214e5a9d8b435750cad12d3c60b0b
Brandon Quach <36943338+brandonq2@users.noreply.github.com>
2018-12-01 22:50:08 -0500

    Updated Demo

--- client/src/Demo.js
+++ client/src/Demo.js
@@ -5,22 +5,51 @@ import {CardData} from './CardData';
 import Grid from "@material-ui/core/Grid";
 import Avatar from "@material-ui/core/Avatar"
 import Footer from './Footer';
+import styled from 'styled-components';
+
 
 const styles = {
     width: 200,
     height: 200,
     margin: 25
 }
+const CodeBlock = styled.section`
+  padding: 2em;
+  background: #C0C0C0;
+  border-radius: 7px;
+`;
+const StyledH2 = styled.h2`
+    padding-top: 10%;
+    margin-top: -10%;
+    color: #7997d6;
+    font-family: Roboto;
+`
+const StyledH1 = styled.h1`
+    padding-top: 10%;
+    margin-top: -10%;
+    color: #5b73b4;
+    font-size: 40px;
+    font-family: Roboto;
+`
+const StyledP = styled.p`
+    font-size: 20px;
+    font-family: Roboto;
+    margin-top: -1%;
+
+`
 export default class Demo extends Component {
     render() {
         return (
             <div>
                 <NavBar/>
-
                 <br/>
                 <br/>
                 <br/>
-                <Grid container justify="center">
+                <br/>
+                <Grid container justify="center" align Items="center">
+                <StyledH1>Demo Page</StyledH1>
+                </Grid>
+                <Grid container justify="center" align Items="center">
                     <Avatar
                         style={styles}
                         src="https://i.imgur.com/nTsc3jv.png"
@@ -39,15 +68,30 @@ export default class Demo extends Component {
                     />
                 </Grid>
                 <hr/>
-                <Grid container spacing={4} style={{minHeight: '50vh'}} justify="center" alignItems="center">
+                <br/>
+                <Grid container justify="center" align Items="center">
+                <StyledH2>Top 3 Animes</StyledH2>
+                </Grid>
+                <Grid container justify="center" align Items="center">
+                <div style={{padding: '0% 12% 0%'}}>
+                <StyledP>
+                These are the undisputed top 3 animes of all time. We have discussed for countless minutes about the top 3 animes, and have eliminated
+                over 5000 other animes that were on our list. We have narrowed them down to the following animes with a perfect score of 5/7:
+                </StyledP>
+                </div>
+                </Grid>
+                <br/>
+                <Grid container spacing={10} style={{minHeight: '70vh'}} justify="center" alignItems="center">
                     <Grid item md={3}>
                         <Card1 CardData={CardData[0]}/>
                     </Grid>
-                    <Grid item md={3} justify="center">
+                    <Grid item md={3}>
                         <Card1 CardData={CardData[2]}/>
+
                     </Grid>
-                    <Grid item md={3} >
+                    <Grid item md={3}>
                         <Card1 CardData={CardData[1]}/>
+
                     </Grid>
                 </Grid>
                 <Footer/>
