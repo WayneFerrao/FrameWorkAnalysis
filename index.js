742df9fdc3b3d73a1882f2afbff72152f03f7a26
Wayne Ferrao <wayne.ferrao93@gmail.com>
2018-12-02 22:04:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -63,13 +63,8 @@ if (process.env.NODE_ENV === 'production') {
     app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 
-        // res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-
     });
 
-    // app.get('/confirm', (req, res) => {
-    //     res.json(CardData);
-    // });
 }
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
